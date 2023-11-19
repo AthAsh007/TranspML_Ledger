85ae3d866c2ae212b2c639aca7a18894b05bb3e9
Jay Shitre <jayshitre@gmail.com>
2023-11-19 15:51:58 +0530

    Modified /upload endpoint to execute the uploaded file

--- transpml_backend/src/__init__.py
+++ transpml_backend/src/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, flash, request, jsonify
 from werkzeug.utils import secure_filename
 from src.database import Codes, db
+import sys
+from io import StringIO
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -20,13 +22,27 @@ def create_app(test_config=None):
             code = Codes (
                 user_account=request.form["user_account"],
                 model_name=file.filename
-            )
+            ) 
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
             db.session.add(code)
             db.session.commit()
+
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            stdout_backup = sys.stdout  # Backup original stdout
+            sys.stdout = StringIO()  # Redirect stdout to a buffer
+            try:
+                exec(open(file_path).read())
+                output = sys.stdout.getvalue()  # Get the output
+            except Exception as e:
+                output = f"Error executing script: {str(e)}"
+            finally:
+                sys.stdout.close()  # Close the redirected buffer
+                sys.stdout = stdout_backup
+
             return jsonify({
                 "user_account": code.user_account,
-                "model_name": file.filename
+                "model_name": file.filename,
+                "output": output
             })
         
     @app.route('/fetch', methods=['GET'])

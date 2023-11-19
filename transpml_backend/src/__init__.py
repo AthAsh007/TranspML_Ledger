ad25a783f33976fb6d604d2a1159865893c7198f
Jay Shitre <jayshitre@gmail.com>
2023-11-19 12:00:01 +0530

    Added Flask backend with file upload endpoint

---
+++ transpml_backend/src/__init__.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, flash, request, jsonify
+from werkzeug.utils import secure_filename
+from src.database import Codes, db
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config == None:
+        app.config.from_mapping(SECRET_KEY="dev",
+                                SQLALCHEMY_DATABASE_URI="sqlite:///codes.db",
+                                UPLOAD_FOLDER="D:/Web Development/Flask/transpml_backend/uploaded_files")
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.route('/upload', methods=['GET', 'POST'])
+    def upload_file():
+        if request.method == 'POST':
+            file = request.files['file']
+            code = Codes (
+                user_account=request.form["user_account"],
+                model_name=file.filename
+            )
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            db.session.add(code)
+            db.session.commit()
+            return jsonify({
+                "user_account": code.user_account,
+                "model_name": file.filename
+            })
+        
+    @app.route('/fetch', methods=['GET'])
+    def fetch_code():
+        data = Codes.query.all()
+        results = {"data": [{
+            "id": code.id,
+            "user_account": code.user_account,
+            "model_name": code.model_name
+        } for code in data]}
+        return jsonify(results)
+
+    db.app = app
+    db.init_app(app)
+    
+    return app
---
+++ transpml_backend/src/database.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Codes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_account = db.Column(db.String(80), nullable=False)
+    model_name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return 'Codes>>>{self.user_account}'
